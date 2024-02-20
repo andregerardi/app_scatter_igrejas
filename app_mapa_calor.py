d41aa1987e029083638f5efb0bfaf89fd1932e64
Dirceu André Gerardi <48441841+andregerardi@users.noreply.github.com>
2024-02-20 18:13:07 -0300

    Update app_mapa_calor.py

--- app_mapa_calor.py
+++ app_mapa_calor.py
@@ -63,7 +63,7 @@ fig.add_layout_image(
 dict(
     source="https://cebrap.org.br/wp-content/themes/cebrap/images/logo-nav.png",
     xref="paper", yref="paper",
-    x=.95, y=.99,
+    x=0.98, y=.1.02,
     sizex=0.1, sizey=0.1,
     xanchor="right", yanchor="bottom"
  )
